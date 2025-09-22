92f4aec84d48af615444bb0e26d98c65e0ad4b0f
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2025-09-22 14:23:02 -0300

    add: aside meny

---
+++ src/components/header/aside.tsx
@@ -0,0 +1,50 @@
+import { memo } from "react";
+
+type NavigationItem = {
+  title: string;
+  url: string;
+};
+
+interface AsideProps extends React.ComponentProps<"aside"> {
+  navigation: NavigationItem[];
+  isOpen?: boolean;
+  setIsOpen?: VoidFunction;
+}
+
+export const AsideComp = ({
+  navigation,
+  isOpen,
+  setIsOpen,
+  ...props
+}: AsideProps) => {
+  return (
+    <aside
+      className={`md:hidden fixed top-0 left-0 h-[100dvh] w-64 bg-eco-light shadow-lg transform transition-transform duration-300 ease-in-out ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } flex flex-col justify-between`}
+      {...props}
+    >
+      <img
+        src="/gnome-1.svg"
+        alt="gnome mascot"
+        onClick={setIsOpen}
+        className="mx-auto mt-2 h-20"
+      />
+
+      <ul className="flex flex-col gap-2  items-center justify-center font-primary font-medium">
+        {navigation.map((item) => (
+          <li
+            key={item.url}
+            className="w-[95%] p-2 hover:bg-eco-mutated/10 text-center mx-auto rounded transition-colors duration-300"
+          >
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="min-h-12"></div>
+    </aside>
+  );
+};
+
+export const Aside = memo(AsideComp);
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,32 +1,60 @@
-import Gnome1 from "../../assets/gnome-1.svg";
+import { useState } from "react";
+import { Aside } from "./aside";
+import { RiMenu3Line } from "react-icons/ri";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleAside = () => setIsOpen((prev) => !prev);
+
   return (
-    <header className="w-full px-4 flex items-center justify-between text-base">
+    <header className="w-full px-2 flex items-center justify-between text-base">
       <div className="flex items-center gap-2">
         <img
-          className="object-contain pointer-events-none"
-          src={Gnome1}
+          className="object-cover pointer-events-none max-h-16"
+          src="/gnome-1.svg"
           alt="gnome mascot"
         />
         <span className="font-primary font-bold">EcoGarden</span>
       </div>
-      <nav>
-        <ul className="flex items-center gap-4 font-primary font-medium">
-          <li className=" cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
-            Sobre
-          </li>
-          <li className=" cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
-            Recursos
-          </li>
-          <li className=" cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
-            Login
-          </li>
-          <li className="py-2 px-4 bg-eco-green-500 rounded-full text-eco-light cursor-pointer transition-colors duration-300 hover:bg-eco-green-400">
-            Localizar
-          </li>
-        </ul>
-      </nav>
+
+      <NavItems />
+
+      <span className="cursor-pointer hover:bg-eco-mutated/10 rounded-full p-2 transition-colors duration-300 md:hidden">
+        <RiMenu3Line onClick={toggleAside} className="size-6" />
+      </span>
+
+      <Aside
+        isOpen={isOpen}
+        navigation={[
+          { title: "Sobre", url: "#about" },
+          { title: "Recursos", url: "#features" },
+          { title: "Login", url: "/login" },
+          { title: "Localizar", url: "/map" },
+        ]}
+        setIsOpen={toggleAside}
+      />
     </header>
+  );
+};
+
+const NavItems = () => {
+  return (
+    <nav className="hidden md:block">
+      <ul className="flex items-center gap-4 font-primary font-medium text-sm">
+        <li className="cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
+          Sobre
+        </li>
+        <li className="cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
+          Recursos
+        </li>
+        <li className="cursor-pointer hover:text-eco-green-400 transition-colors duration-300">
+          Login
+        </li>
+        <li className="py-2 px-4 bg-eco-green-500 rounded-full text-eco-light cursor-pointer transition-colors duration-300 hover:bg-eco-green-400">
+          Localizar
+        </li>
+      </ul>
+    </nav>
   );
 };
